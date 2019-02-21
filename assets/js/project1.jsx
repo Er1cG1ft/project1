e6c7fb4fedf6c9c3b3621eb67eb4b08b17352459
Eric Gift <giftsoccer14@gmail.com>
2019-02-21 01:10:35 +0000

    actual working movement

--- assets/js/project1.jsx
+++ assets/js/project1.jsx
@@ -24,8 +24,8 @@ class Project1 extends React.Component {
     this.speed = 2;
     this.friction = 0.98;
     this.keys = [];
-    window.addEventListener('keyup', (e) => {this.keys[e.keyCode] = true});
-    window.addEventListener('keydown', (e) => {this.keys[e.keyCode] = false});
+    window.addEventListener('keyup', (e) => {this.keys[e.keyCode] = false});
+    window.addEventListener('keydown', (e) => {this.keys[e.keyCode] = true});
     this.channel
         .join()
         .receive("ok",  this.got_view.bind(this))
@@ -65,13 +65,11 @@ class Project1 extends React.Component {
     this.board.save();
     this.board.fillStyle = 'gray';
     this.board.fillRect(0, 0, this.width, this.height);
-    //this.board.translate(position[1], position[0]);
     this.board.strokeStyle = '#ffffff';
     this.board.fillStyle = '#000000';
     this.board.lineWidth = 2;
     this.board.beginPath();
-    //this.board.arc(0, 0, 50, 0, 2 * Math.PI);
-    this.board.arc(position[1], position[0], 5, 0, Math.PI * 2);
+    this.board.arc(position.x, position.y, 5, 0, Math.PI * 2);
     this.board.closePath();
     this.board.fill();
     this.board.stroke();
@@ -79,73 +77,49 @@ class Project1 extends React.Component {
   }
   
   update() {
-    //left
-    if (this.keys[40]) {
-      if (this.velX > -this.speed) {
-          this.velX--;
-      }
-    }
-    //right
+    requestAnimationFrame(() => {this.update()});
     if (this.keys[38]) {
-      if (this.velX < this.speed) {
-          this.velX++;
-      }
-    }
-    //up 
-    if (this.keys[39]) {
       if (this.velY > -this.speed) {
           this.velY--;
       }
     }
-    //down
-    if (this.keys[37]) {
+    if (this.keys[40]) {
       if (this.velY < this.speed) {
           this.velY++;
       }
     }
+    if (this.keys[39]) {
+      if (this.velX < this.speed) {
+          this.velX++;
+      }
+    }
+    if (this.keys[37]) {
+      if (this.velX > -this.speed) {
+          this.velX--;
+      }
+    }
     
     this.velY *= this.friction;
-    this.player.position[1] += this.velY;
+    this.player.position.y += this.velY;
     this.velX *= this.friction;
-    this.player.position[0] += this.velX;
+    this.player.position.x += this.velX;
 
-    if (this.player.position[1] >= this.width - 5) {
-        this.player.position[1] = this.width - 5;
-    } else if (this.player.position[1] <= 5) {
-        this.player.position[1] = 5;
+    if (this.player.position.x >= this.width - 5) {
+        this.player.position.x = this.width - 5;
+    } else if (this.player.position.x <= 5) {
+        this.player.position.x = 5;
+    }
+    if (this.player.position.y > this.height - 5) {
+        this.player.position.y = this.height - 5;
+    } else if (this.player.position.y <= 5) {
+        this.player.position.y = 5;
     }
 
-    if (this.player.position[0] > this.height - 5) {
-        this.player.position[0] = this.height - 5;
-    } else if (this.player.position[0] <= 5) {
-        this.player.position[0] = 5;
-    }
     this.renderPlayer(this.player.position);
-    
 
-    // Next frame
-    requestAnimationFrame(() => {this.update()});
   }
   
   startGame(){
-    // this.setState({
-    //   inGame: true,
-    //   currentScore: 0,
-    // });
-    // Make ship
-    // let ship = new Ship({
-    //   position: {
-    //     x: this.state.screen.width/2,
-    //     y: this.state.screen.height/2
-    //   },
-    //   create: this.createObject.bind(this),
-    //   onDie: this.gameOver.bind(this)
-    // });
-    // this.createObject(ship, 'ship');
-
-    // // Make asteroids
-    // this.asteroids = [];
-    // this.generateAsteroids(this.state.asteroidCount)
   }
   
   movePlayer(e) {
